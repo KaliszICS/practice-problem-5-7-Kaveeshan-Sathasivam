ea9927adc79ffb530be4800cd9ca1178212039c9
Kaveeshan-Sathasivam <343279329@tdsb.ca>
2026-05-19 18:13:43 +0000

    djajdwj

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,38 @@
+/*File: Practice problem 5.7
+Author: Kaveeshan
+Date Created: May 19, 2026
+Date Last Modified: May 19, 2026 */
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void swap(ArrayList<String> list, int index1, int index2) {
+        String temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
 
-	}
+    public static ArrayList<Double> createArrayList(double[] arr) {
+        ArrayList<Double> list = new ArrayList<Double>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+
+    public static ArrayList<Integer> mergeLists(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> merged = new ArrayList<Integer>();
+        for (int i = 0; i < list1.size(); i++) {
+            merged.add(list1.get(i));
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            merged.add(list2.get(i));
+        }
+        return merged;
+    }
+
+    public static void main(String args[]) {
+
+    }
 
 }
